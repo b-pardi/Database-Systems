7282a863a7a88bc93b73c33bdfe6f1df92c75308
Brandon Pardi <brandonpardi24@gmail.com>
2022-09-06 12:32:04 -0700

    refactored with function to check and append data

--- project/main.py
+++ project/main.py
@@ -19,9 +19,32 @@ air_forecast_dict = {'City':[], 'Time Updated':[], 'AQI':[], 'pm10':[], 'pm2.5':
 # other city info
 
 
+'''
+API has some data missing for some cities, function checks if data is available for each city,
+and if it is appends it to the 'dest' dictionary.
+'''
+def tryAppend(dest_dict, dest_key, src_dict, src_keys):
+    i = 1
+    data = src_dict[src_keys[0]]
+    while i < len(src_keys):
+        if src_keys != None:
+            try:
+                data = data[src_keys[i]]
+            except:
+                data = 'na'
+        i+=1
+
+    # checks to make sure data is only 1 item, not a dictionary
+    # data will be not a dict if the data sought after is available
+    if not isinstance(data, dict):
+        dest_dict[dest_key].append(data)
+    
+
+
 # filling request and scraping API data for each city,
 # putting into dictionary, then into dataframe
-for i in range(9):
+num_cities = 10
+for i in range(num_cities):
     try:
         # send API request
         r = requests.get(f"https://api.waqi.info/feed/{loc[i]}/?token={key}")
@@ -42,55 +65,32 @@ for i in range(9):
     Appending data from AQI API for CURRENT air quality
     '''
     air_dict['City'].append(loc[i])
-    air_dict['lat'].append(air_data['city']['geo'][0])
-    air_dict['long'].append(air_data['city']['geo'][1])
-    air_dict['Time Updated'].append(air_data['time']['s'])
-    air_dict['AQI'].append(air_data['aqi'])
+    tryAppend(air_dict, 'lat', air_data, ('city', 'geo', 0))
+    tryAppend(air_dict, 'long', air_data, ('city', 'geo', 1))
+    tryAppend(air_dict, 'Time Updated', air_data, ('time', 's'))
+    tryAppend(air_dict, 'AQI', air_data, ('aqi',))
 
-    # some cities, not all this information is available
-    try:
-        air_dict['CO'].append(air_data['iaqi']['co']['v'])
-    except:
-        air_dict['CO'].append('na')
-    try:
-        air_dict['H'].append(air_data['iaqi']['h']['v'])
-    except:
-        air_dict['H'].append('na')
-    try:
-        air_dict['NO2'].append(air_data['iaqi']['no2']['v'])
-    except:
-        air_dict['NO2'].append('na')
-    try:
-        air_dict['Ozone'].append(air_data['iaqi']['o3']['v'])
-    except:
-        air_dict['Ozone'].append('na')
-    try:
-        air_dict['pm10'].append(air_data['iaqi']['pm25']['v'])
-    except:
-        air_dict['pm10'].append('na')
-    try:
-        air_dict['pm2.5'].append(air_data['iaqi']['pm25']['v'])
-    except:
-        air_dict['pm2.5'].append('na')
+    # some cities, not all this information is available, explaining the try/except in function
+    tryAppend(air_dict, 'CO', air_data, ('iaqi', 'co', 'v'))
+    tryAppend(air_dict, 'H',  air_data, ('iaqi', 'h', 'v'))
+    tryAppend(air_dict, 'NO2',  air_data, ('iaqi', 'no2', 'v'))
+    tryAppend(air_dict, 'Ozone',  air_data, ('iaqi', 'o3', 'v'))
+    tryAppend(air_dict, 'pm10', air_data, ('iaqi', 'pm10', 'v'))
+    tryAppend(air_dict, 'pm2.5',  air_data, ('iaqi', 'pm25', 'v'))    
 
 
     '''
     Appending data from AQI API for 3 day FORECASTED air quality
     '''
     air_forecast_dict['City'].append(loc[i])
-    air_forecast_dict['Time Updated'].append(air_data['time']['s'])
-    air_forecast_dict['AQI'].append(air_data['aqi'])
-    try:
-        air_forecast_dict['pm10'].append(air_data['iaqi']['pm25']['v'])
-    except:
-        air_forecast_dict['pm10'].append('na')
-    try:
-        air_forecast_dict['pm2.5'].append(air_data['iaqi']['pm25']['v'])
-    except:
-        air_forecast_dict['pm2.5'].append('na')
+    tryAppend(air_forecast_dict, 'Time Updated', air_data, ('time', 's'))
+    tryAppend(air_forecast_dict, 'AQI', air_data, ('aqi',))
+    
 
+    tryAppend(air_forecast_dict, 'pm10', air_data, ('forecast', 'daily', 'pm10', 5, 'avg'))
+    tryAppend(air_forecast_dict, 'pm2.5', air_data, ('forecast', 'daily', 'pm25', 5, 'avg'))
 
 aqi_df = pd.DataFrame(air_dict)
-forecasted_aqi_df = pd.DataFrame(air_dict)
+forecasted_aqi_df = pd.DataFrame(air_forecast_dict)
 print(aqi_df.head())
 print(forecasted_aqi_df.head())
