e99ff4b730bb61457f5ec056ab54d6cca362b57b
Brandon Pardi <brandonpardi24@gmail.com>
2022-09-07 13:28:07 -0700

    lab-2 bulk loading completed

--- lab-2/load-tpch.sql
+++ lab-2/load-tpch.sql
@@ -1,9 +1,10 @@
-BULK INSERT customer
-FROM '.csv'
-with (
-    FORMAT = 'CSV',
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n'
-)
-
+.mode csv
+.separator '|'
+.import data/customer.tbl customer
+.import data/lineitem.tbl lineitem
+.import data/nation.tbl nation
+.import data/orders.tbl orders
+.import data/part.tbl part
+.import data/partsupp.tbl partsupp
+.import data/region.tbl region
+.import data/supplier.tbl supplier
