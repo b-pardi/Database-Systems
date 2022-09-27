c87526fa652bd82b6c17bb03edd79b6d7714d3e7
Brandon Pardi <brandonpardi24@gmail.com>
2022-09-27 12:01:29 -0700

    prof help q3 lab5

--- lab-5/test/3.sql
+++ lab-5/test/3.sql
@@ -1,12 +1,14 @@
 -- For the line items ordered in October 1996 (o orderdate), find the smallest discount that is larger
 -- than the average discount among all the orders.
 
-SELECT MIN(l_discount)
 
+SELECT min(l_discount)
 FROM lineitem, orders
-
 WHERE l_orderkey = o_orderkey
-AND o_orderdate LIKE '1996-10%'
-
-GROUP BY 
-HAVING l_discount > AVG(l_discount)
+    AND o_orderdate LIKE '1996-10%'
+    and l_discount >
+        (select avg(o_discount)
+        from
+            (select l_orderkey, avg(l_discount) as o_discount
+            from lineitem
+            group by l_orderkey) sq1);
